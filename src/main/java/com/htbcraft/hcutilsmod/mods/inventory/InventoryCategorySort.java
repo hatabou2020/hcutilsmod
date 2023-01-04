3ddff2b98bc87935eecfd18f453275aafdd64b48
hatabou2020 <hatabou.minecraft@gmail.com>
2023-01-05 00:31:12 +0900

    1.19.3でCreativeModeTabが変わったためカテゴリーソートの実装を修正した

--- src/main/java/com/htbcraft/hcutilsmod/mods/inventory/InventoryCategorySort.java
+++ src/main/java/com/htbcraft/hcutilsmod/mods/inventory/InventoryCategorySort.java
@@ -1,17 +1,42 @@
 package com.htbcraft.hcutilsmod.mods.inventory;
 
-import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class InventoryCategorySort implements Comparator<ItemStack> {
     private static final Logger LOGGER = LogManager.getLogger();
-    private final NonNullList<ItemStack> items = NonNullList.create();
+
+    /* クリエイティブモードのカテゴリ順でソートする
+     *  1.建築ブロック
+     *  2.色付きブロック
+     *  3.天然ブロック
+     *  4.機能的ブロック
+     *  5.レッドストーン
+     *  6.道具と実用品
+     *  7.戦闘
+     *  8.食べ物と飲み物
+     *  9.材料
+     * 10.スポーンエッグ
+     */
+    private static final List<CreativeModeTab> TABS = Arrays.asList(
+        CreativeModeTabs.BUILDING_BLOCKS,
+        CreativeModeTabs.COLORED_BLOCKS,
+        CreativeModeTabs.NATURAL_BLOCKS,
+        CreativeModeTabs.FUNCTIONAL_BLOCKS,
+        CreativeModeTabs.REDSTONE_BLOCKS,
+        CreativeModeTabs.TOOLS_AND_UTILITIES,
+        CreativeModeTabs.COMBAT,
+        CreativeModeTabs.FOOD_AND_DRINKS,
+        CreativeModeTabs.INGREDIENTS,
+        CreativeModeTabs.SPAWN_EGGS
+    );
 
     @Override
     public int compare(ItemStack o1, ItemStack o2) {
@@ -37,25 +62,7 @@ public class InventoryCategorySort implements Comparator<ItemStack> {
 
         int ret = 0;
 
-        /* クリエイティブモードのカテゴリ順でソートする
-         * 1.建築ブロック
-         * 2.装飾ブロック
-         * 3.レッドストーン
-         * 4.運送
-         * 5.その他
-         * 6.食料
-         * 7.道具
-         * 8.戦闘
-         * 9.醸造
-         */
-        for (CreativeModeTab tab : CreativeModeTabs.tabs()) {
-            // 余分なタブは省く
-            if ((tab == CreativeModeTabs.SEARCH) ||
-                (tab == CreativeModeTabs.HOTBAR) ||
-                (tab == CreativeModeTabs.INVENTORY)) {
-                continue;
-            }
-
+        for (CreativeModeTab tab : TABS) {
             for (ItemStack item : tab.getDisplayItems()) {
                 if (o1.sameItem(item)) {
                     ret = -1;
